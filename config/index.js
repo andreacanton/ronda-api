b567113ccf0b93a4d2244811d9e97b299ec95e01
Andrea Canton <andrea.canton@gmail.com>
2020-12-08 12:47:20 +0100

    Add basic configuration

---
+++ config/index.js
@@ -0,0 +1,70 @@
+var convict = require('convict');
+
+convict.addFormat(require('convict-format-with-validator').ipaddress);
+
+var config = convict({
+  env: {
+    doc: 'The application environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV',
+  },
+  ip: {
+    doc: 'The IP address to bind.',
+    format: 'ipaddress',
+    default: '127.0.0.1',
+    env: 'IP_ADDRESS',
+  },
+  port: {
+    doc: 'The port to bind.',
+    format: 'port',
+    default: 3000,
+    env: 'PORT',
+    arg: 'port',
+  },
+  db: {
+    host: {
+      doc: 'Database host name/IP',
+      format: '*',
+      default: 'localhost',
+      env: 'DATABASE_HOST',
+    },
+    dialect: {
+      doc: 'Database dialect',
+      format: '*',
+      default: 'sqlite',
+      env: 'DATABASE_DIALECT',
+    },
+    port: {
+      doc: 'The port the db server',
+      format: 'port',
+      default: 3306,
+      env: 'DATABASE_PORT',
+    },
+    name: {
+      doc: 'Database name',
+      format: String,
+      default: 'ronda',
+      env: 'DATABASE_NAME',
+    },
+    user: {
+      doc: 'Database user',
+      format: String,
+      default: 'ronda',
+      env: 'DATABASE_USER',
+    },
+    password: {
+      doc: 'Database password',
+      format: String,
+      default: 'ronda',
+      env: 'DATABASE_PASSWORD',
+    },
+  },
+});
+
+var env = config.get('env');
+config.loadFile(`./config/${env}.json`); // TODO: Check if file exists
+
+config.validate({ allowed: 'strict' });
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var config = require('./config/');
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+});
+
+server.listen(config.get('port'), config.get('ip'), function (x) {
+  var addy = server.address();
+  console.log('running on http://' + addy.address + ':' + addy.port);
+});
