303c4f5b2db936369ad2e47acd339dab8e1f36b9
Andrea Canton <andrea.canton@gmail.com>
2021-02-13 22:09:23 +0100

    add some tests

--- db/test.js
+++ db/test.js
@@ -1,9 +1,16 @@
 const orm = require('./index');
 
-test('Orm should be defined', () => {
-  expect(orm).toBeDefined();
-});
-
-test('Knex should be defined', () => {
-  expect(orm.knex).toBeDefined();
+describe('database', () => {
+  test('Orm should be defined', () => {
+    expect(orm).toBeDefined();
+  });
+  test('Knex should be defined', () => {
+    expect(orm.knex).toBeDefined();
+  });
+  test('orm should create model', () => {
+    const Migrations = orm.model('Migrations', {
+      tableName: 'knex_migrations',
+    });
+    expect(Migrations).toBeDefined();
+  });
 });
--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+const logger = require('./logger');
+
 test('NODE_ENV expect to be test', () => {
   expect(process.env.NODE_ENV).toBe('test');
+});
+
+test('Should log', () => {
+  logger.debug('debug message');
+  logger.info('info message');
+  logger.warn('warn message');
+  logger.error('error message');
+  expect(1).toBe(1);
 });
