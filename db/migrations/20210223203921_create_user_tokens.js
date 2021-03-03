57ff98d16e6c9e4225fd31e5f8bebdcc59e7f6fa
Andrea Canton <andrea.canton@gmail.com>
2021-03-03 22:32:41 +0100

    rename create_user_token migration

--- db/migrations/20210223203921_create_user_tokens.js
+++ db/migrations/20210223203921_create_user_tokens.js
@@ -1,11 +1,13 @@
 const { v4: uuidV4 } = require('uuid');
 
 exports.up = (knex) =>
-  knex.schema.createTable('tokens', (t) => {
+  knex.schema.createTable('user_tokens', (t) => {
     t.string('token_id', 36).primary().defaultTo(uuidV4());
+    t.string('user_id', 36);
     t.string('type').notNullable();
     t.datetime('expiration').notNullable();
     t.timestamps(false, true);
+    t.foreign('user_id').references('user_id').inTable('users');
   });
 
 exports.down = (knex) => knex.schema.dropTableIfExists('tokens');
