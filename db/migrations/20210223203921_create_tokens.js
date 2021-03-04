3c5b262dac7b298f8965e5b17d298af05c5b5d28
Andrea Canton <andrea.canton@gmail.com>
2021-03-04 20:12:44 +0100

    rename token table

--- db/migrations/20210223203921_create_tokens.js
+++ db/migrations/20210223203921_create_tokens.js
@@ -1,5 +1,5 @@
 exports.up = (knex) =>
-  knex.schema.createTable('user_tokens', (t) => {
+  knex.schema.createTable('tokens', (t) => {
     t.string('token_id', 36).primary();
     t.string('user_id', 36);
     t.string('type').notNullable();
@@ -8,4 +8,4 @@ exports.up = (knex) =>
     t.foreign('user_id').references('user_id').inTable('users');
   });
 
-exports.down = (knex) => knex.schema.dropTableIfExists('user_tokens');
+exports.down = (knex) => knex.schema.dropTableIfExists('tokens');
