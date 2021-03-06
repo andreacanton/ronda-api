ab0697d720258eec5acdfd21a4aa5d42d7a32a4e
Andrea Canton <andrea.canton@gmail.com>
2021-03-06 14:02:40 +0100

    add auth test

---
+++ test/auth.test.js
@@ -0,0 +1,55 @@
+const supertest = require('supertest');
+const app = require('../app');
+
+const request = supertest(app);
+
+describe('POST /auth/login', () => {
+  it('responds 200 with json', (done) => {
+    request
+      .post('/auth/login')
+      .send({
+        identity: 'asurname@test.com',
+        password: 'password01',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.access_token).toBeDefined();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('responds 401 with wrong password', (done) => {
+    request
+      .post('/auth/login')
+      .send({
+        identity: 'asurname@test.com',
+        password: 'password02',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .then((response) => {
+        expect(response.body.message).toBeDefined();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('responds 401 with wrong identity', (done) => {
+    request
+      .post('/auth/login')
+      .send({
+        identity: 'asurnsame@test.com',
+        password: 'password01',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .then((response) => {
+        expect(response.body.message).toBeDefined();
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
