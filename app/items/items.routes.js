f6fe9e2b0e15194b775f2ac637f0e160b0cd4074
Andrea Canton <andrea.canton@gmail.com>
2021-03-28 17:47:37 +0200

    add consistency to camelCase in items response

--- app/items/items.routes.js
+++ app/items/items.routes.js
@@ -6,7 +6,15 @@ const routes = express.Router();
 
 routes.get('/', authorize(), async (res, req) => {
   const items = await orm.knex.from('items');
-  req.json(items);
+  req.json(
+    items.map((item) => ({
+      itemId: item.item_id,
+      name: item.name,
+      sizeCategory: item.size_category,
+      createdAt: item.created_at,
+      updatedAt: item.updated_at,
+    })),
+  );
 });
 
 module.exports = routes;
