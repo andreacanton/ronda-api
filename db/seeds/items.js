b6e72efbed1531064a127c87ba49cdb1ed04436f
Andrea Canton <andrea.canton@gmail.com>
2021-04-05 17:33:34 +0200

    fix items seed

--- db/seeds/items.js
+++ db/seeds/items.js
@@ -1,20 +1,24 @@
-module.exports = async (knex) =>
-  knex('items')
-    .truncate()
-    .then(() =>
-      knex('items').insert([
-        { name: 'Maglietta', size_category: 'top' },
-        { name: 'Maglione', size_category: 'top' },
-        { name: 'Felpa', size_category: 'top' },
-        { name: 'Giubbotto', size_category: 'top' },
-        { name: 'Canottiera', size_category: 'top' },
-        { name: 'Scarpe', size_category: 'shoes' },
-        { name: 'Pantaloni', size_category: 'trousers' },
-        { name: 'Mutande', size_category: 'bottom' },
-        { name: 'Reggiseno', size_category: 'bra' },
-        { name: 'Zaino' },
-        { name: 'Berretto' },
-        { name: 'Sacco a pelo' },
-        { name: 'Coperta' },
-      ]),
+module.exports = (knex) =>
+  knex('order_items')
+    .del()
+    .then(
+      knex('items')
+        .del()
+        .then(() =>
+          knex('items').insert([
+            { name: 'Maglietta', size_category: 'top' },
+            { name: 'Maglione', size_category: 'top' },
+            { name: 'Felpa', size_category: 'top' },
+            { name: 'Giubbotto', size_category: 'top' },
+            { name: 'Canottiera', size_category: 'top' },
+            { name: 'Scarpe', size_category: 'shoes' },
+            { name: 'Pantaloni', size_category: 'trousers' },
+            { name: 'Mutande', size_category: 'bottom' },
+            { name: 'Reggiseno', size_category: 'bra' },
+            { name: 'Zaino' },
+            { name: 'Berretto' },
+            { name: 'Sacco a pelo' },
+            { name: 'Coperta' },
+          ]),
+        ),
     );
