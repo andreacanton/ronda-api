89f4354d9e6afde986f1b212d02a95df1e61f216
Andrea Canton <andrea.canton@gmail.com>
2021-04-05 17:55:55 +0200

    order items model

---
+++ app/order/order-item.model.js
@@ -0,0 +1,56 @@
+const _ = require('lodash');
+const CheckIt = require('checkit');
+const orm = require('../../db');
+
+const OrderItem = orm.model(
+  'OrderItem',
+  {
+    tableName: 'order_items',
+    idAttribute: 'order_item_id',
+    initialize() {
+      this.on('saving', this.validateSave);
+    },
+    validateSave() {
+      return CheckIt({
+        orderId: [
+          'required',
+          'integer',
+          async (value) => {
+            const response = await orm.knex
+              .from('orders')
+              .where('order_id', '=', value);
+            return response.length > 0;
+          },
+        ],
+        itemId: [
+          'required',
+          'integer',
+          async (value) => {
+            const response = await orm.knex
+              .from('items')
+              .where('item_id', '=', value);
+            return response.length > 0;
+          },
+        ],
+        size: ['required'],
+        genre: ['required', (value) => ['male', 'female'].includes(value)],
+        quantity: ['required', 'integer', 'min:1'],
+      }).run(this.attributes);
+    },
+    parse(response) {
+      return _.mapKeys(response, (value, key) => _.camelCase(key));
+    },
+    format(attributes) {
+      return _.mapKeys(attributes, (value, key) => _.snakeCase(key));
+    },
+    order() {
+      return this.belongsTo('Order', 'order_id', 'order_id');
+    },
+    item() {
+      return this.belongsTo('Item', 'item_id', 'item_id');
+    },
+  },
+  {},
+);
+
+module.exports = OrderItem;
--- db/migrations/20210405110933_create_order_items.js
+++ db/migrations/20210405110933_create_order_items.js
@@ -1,12 +1,13 @@
 exports.up = (knex) =>
   knex.schema.createTable('order_items', (t) => {
+    t.increments('order_item_id').primary();
     t.integer('order_id').unsigned().notNullable();
     t.integer('item_id').unsigned().notNullable();
     t.string('size');
     t.string('genre');
     t.integer('quantity');
     t.text('notes');
-    t.primary(['order_id', 'item_id']);
+    t.unique(['order_id', 'item_id']);
     t.foreign('order_id').references('order_id').inTable('orders');
     t.foreign('item_id').references('item_id').inTable('items');
   });
