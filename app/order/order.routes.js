178d4b8fc8da8beea7856fe5863db94f5d60cb00
Andrea Canton <andrea.canton@gmail.com>
2021-04-08 22:49:06 +0200

    add order creation route

--- app/order/order.routes.js
+++ app/order/order.routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const { authorize } = require('../authorization/auth.middleware');
+const orm = require('../../db');
+
 const Order = require('./order.model');
+const OrderItem = require('./order-item.model');
+const OrderNote = require('./order-note.model');
 
 const routes = express.Router();
 
@@ -15,6 +19,43 @@ routes.get('/', authorize(), async (req, res) => {
     direction: req.query.dir,
   });
   res.json(orders);
+});
+
+routes.post('/', authorize(), async (req, res, next) => {
+  try {
+    const params = req.body;
+    const order = await orm.transaction((t) =>
+      new Order({
+        recipientId: params.recipientId,
+        destination: params.destination,
+        status: 'created',
+      })
+        .save(null, { transacting: t })
+        .tap((model) =>
+          Promise.map(params.items, (item) =>
+            new OrderItem(item).save(
+              { orderId: model.orderId },
+              { transacting: t },
+            ),
+          ),
+        )
+        .tap((model) =>
+          new OrderNote(params.note).save(
+            { orderId: model.orderId, phase: 'creation' },
+            { transacting: t },
+          ),
+        ),
+    );
+    res.json(order);
+  } catch (e) {
+    if (/invalid value/.test(e.message)) {
+      res.status(400).json({
+        error: e,
+      });
+    } else {
+      next(e);
+    }
+  }
 });
 
 module.exports = routes;
